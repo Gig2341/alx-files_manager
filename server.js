ba0ff8d77bd07cdcf0741a669f584da0ef07a37a
Daniel Kofi Frempong <55427509+Gig2341@users.noreply.github.com>
2024-02-14 13:04:01 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+// use body-parser to parse JSON data sent in request body
+
+// port
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware to parse JSON bodies
+const parser = app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log('Server running on port 5000');
+});
+
+module.exports = parser;
